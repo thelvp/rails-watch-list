4021f2a08594e315ab79da5c26d2cebf5b646a3f
Loes van Puijenbroek <loesvanpuijenbroek@gmail.com>
2024-02-22 11:46:43 +0100

    list - show

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
-  resources :lists, only: [:index]
+  resources :lists, only: [:index, :show]
 
 end
 
